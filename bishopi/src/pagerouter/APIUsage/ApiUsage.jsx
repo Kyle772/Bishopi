d46637ccb7c9a8d1d3486a347bd857de4d122480
Kyle D <contact@KyleDiggs.com>
2020-07-19 14:57:19 -0700

    Basic Router Setup

---
+++ bishopi/src/pagerouter/APIUsage/ApiUsage.jsx
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import { useLocation, withRouter } from 'react-router-dom';
+
+class ApiUsagePage extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+
+  componentDidMount() {}
+  
+  render () {
+    }
+}
+
+export default withRouter(ApiUsagePage);
---
+++ bishopi/src/pagerouter/Pricing/Pricing.jsx
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import { useLocation, withRouter } from 'react-router-dom';
+
+class PricingPage extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+
+  componentDidMount() {}
+  
+  render () {
+    }
+}
+
+export default withRouter(PricingPage);
