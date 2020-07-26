d205d986230005b56102707b03c207cff74e1dc0
Kyle D <contact@KyleDiggs.com>
2020-07-26 15:48:57 -0700

    finished setting up new template code and replacing old

--- bishopi/src/App.js
+++ bishopi/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery';
 import Home from './PageRouter/Home/Home';
 import ApiUsage from './PageRouter/APIUsage/ApiUsage';
 import Billing from './PageRouter/Billing/Billing';
@@ -10,10 +9,7 @@ import Support from './PageRouter/Support/Support';
 import Account from './PageRouter/Account/Account';
 import ProfileEdit from './PageRouter/EditProfile/EditProfile';
 import bootstrap from 'react-bootstrap'
-import './Assets/css/style.css';
-import './Assets/css/responsive.css';
 import './App.css';
-import './Assets/js/main.js';
 
 import {
     BrowserRouter as Router,
